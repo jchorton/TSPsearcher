750e33db92d39e478f8e176071b3b31ad9f203e4
Rufian55 <kearnsc@oregonstate.edu>
2017-03-14 13:18:52 -0400

    Production Christofides

--- TSP.cpp
+++ TSP.cpp
@@ -7,14 +7,30 @@
 ******************************************************************************/
 #include "TSP.h"
 
-// TSP Class constructor.
-TSP::TSP(string inFile, string outFile) {
-	inFileName = inFile;
-	outFileName = outFile;
+struct thread_data {
+	int tid;
+	TSP *tsp;
+};
+struct thread_data *data;
 
-	getCityCount();
+TSP::TSP(string in, string out) {
+	inFileName = in;
+	outFileName = out;
 
-	// Allocate memory and initialize graph.
+	int count = 0;
+	ifstream inStream;
+	inStream.open(inFileName.c_str(), ios::in);
+
+	if (!inStream) {
+		cout << "Can't open input file " << inFileName << "\n";
+		exit(1);
+	}
+	std::string unused;
+	while (std::getline(inStream, unused))
+		++count;
+	V = count;
+	inStream.close();
+
 	graph = new int*[V];
 	for (int i = 0; i < V; i++) {
 		graph[i] = new int[V];
@@ -30,7 +46,6 @@ TSP::TSP(string inFile, string outFile) {
 	adjacencyList = new vector<int>[V];
 };
 
-// TSP Class destructor (memory manager).
 TSP::~TSP() {
 	for (int i = 0; i < V; i++) {
 		delete[] graph[i];
@@ -41,37 +56,19 @@ TSP::~TSP() {
 	delete[] adjacencyList;
 }
 
-// Returns node V (city) count in input file and updates TSP::V.
-void TSP::getCityCount() {
-	int numLines = 0;
-	ifstream inStream;
-	inStream.open(inFileName.c_str(), ios::in);
-
-	if (!inStream) {
-		cout << "Error: TSP.cpp failed to open input file " << inFileName << "\n";
-		exit(1);
-	}
-	string dummy;
-	while (std::getline(inStream, dummy))
-		numLines++;
-	V = numLines;
-	inStream.close();
-};
-
-/* Reads X and Y coordinates from input file and updates cities vector.  Assumes input
-   file is sorted by city int as cityNum is discarded here. */
-void TSP::readCities() {
+void TSP::readCityData() {
 	ifstream inStream;
 	inStream.open(inFileName.c_str(), ios::in);
 	if (!inStream) {
-		cout << "Error: TSP.cpp failed to open input file " << inFileName << "\n";
+		cout << "Can't open input file " << inFileName << "\n";
 		exit(1);
 	}
-	int cityNum, X, Y, i = 0;
+	int c, x, y;
+	int i = 0;
 	while (!inStream.eof()) {
-		inStream >> cityNum >> X >> Y; // cityNum discarded.
-		struct City city = { X, Y };
-		cities.push_back(city);
+		inStream >> c >> x >> y;
+		struct City c = { x, y };
+		cities.push_back(c);
 		i++;
 	}
 	inStream.close();
@@ -82,22 +79,16 @@ int TSP::calcDistance(struct TSP::City cityA, struct TSP::City cityB) {
 	return round(sqrt(pow(cityA.X - cityB.X, 2) + pow(cityA.Y - cityB.Y, 2)));
 };
 
-// Utility struct used for managing thread assignment to instances of TSP class.
-struct threadData { int tid; TSP *tsp; };
-struct threadData *data;
-
-// Populates the graph matrix with calculated distances.  Called by loadMatrix().
-void *populateMatrix(void* args) {
-	struct threadData *_data = (struct threadData *) args;
-	int tid = _data->tid;
-	TSP *tsp = _data->tsp;
+void *F(void* args) {
+	struct thread_data *my_data = (struct thread_data *) args;
+	int tid = my_data->tid;
+	TSP *tsp = my_data->tsp;
 	int **graph = tsp->graph;
 	int start, end;
 
-	start = tsp->startIndex[tid];
+	start = tsp->beginIndex[tid];
 	end = tsp->endIndex[tid];
 
-	// Fill matrix with ALL edge distances.
 	for (int i = start; i <= end; i++) {
 		for (int j = i; j < tsp->V; j++) {
 			graph[i][j] = graph[j][i] = tsp->calcDistance(tsp->cities[i], tsp->cities[j]);
@@ -106,321 +97,282 @@ void *populateMatrix(void* args) {
 	pthread_exit(NULL);
 }
 
-// Populates graph matrix via threading routines. Runs O(~n^3) [look at populateMatrix].
-void TSP::loadMatrix(int threadGuard) {
-	int someThreads = (V / threadGuard) * 0.25;
-	int temp = (V / threadGuard) - someThreads;
-	int remaining = V - (temp * threadGuard);
-	int theRest = threadGuard / 2 + 1;
+void TSP::loadMatrix() {
+	int amount = (V / NUM_THREADS) * 0.2;
+	int x = (V / NUM_THREADS) - amount;
+	int rem = V - (x * NUM_THREADS);
+	int half = NUM_THREADS / 2 + 1;
 
-	int index = 0;
-	for (int i = 0; i < theRest; i++) {
-		startIndex[i] = index;
-		index += (temp - 1);
-		endIndex[i] = index;
-		index++;
+	int pos = 0;
+	for (int i = 0; i < half; i++) {
+		beginIndex[i] = pos;
+		pos += (x - 1);
+		endIndex[i] = pos;
+		pos++;
 	}
-	int remainingThreads = threadGuard - theRest + 1;
-	int give2each = remaining / remainingThreads;
-
-	// Distribute remaining threads.
-	for (int i = theRest; i < threadGuard; i++) {
-		startIndex[i] = index;
-		index += (temp + give2each - 1);
-		endIndex[i] = index;
-		index++;
+	int remainingThreads = NUM_THREADS - half + 1;
+	int extraToEach = rem / remainingThreads;
+	for (int i = half; i < NUM_THREADS; i++) {
+		beginIndex[i] = pos;
+		pos += (x + extraToEach - 1);
+		endIndex[i] = pos;
+		pos++;
 	}
-	endIndex[threadGuard - 1] = V - 1;
+	endIndex[NUM_THREADS - 1] = V - 1;
 
-	int rc;
-	void *threadStatus;
-	data = new struct threadData[V];
+	int rc; void *status;
+	data = new struct thread_data[V];
 
-	// Allocate memory for V tid's.
 	pthread_t *thread = new pthread_t[V];
 	pthread_attr_t attr;
 
-	// Initialize/set thread detached state attribute.
 	pthread_attr_init(&attr);
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
-	// Create thread(s).
-	for (int i = 0; i < threadGuard; i++) {
-		data[i].tid = i;
-		data[i].tsp = this;
-		rc = pthread_create(&thread[i], &attr, populateMatrix, (void*)&data[i]);
+	for (long t = 0; t < NUM_THREADS; t++) {
+		data[t].tid = t;
+		data[t].tsp = this;
+		rc = pthread_create(&thread[t], &attr, F, (void*)&data[t]);
 		if (rc) {
-			cout << "ERROR: pthread_create() returned: " << rc << "\n";
-			exit(1);
+			printf("ERROR; return code from pthread_create() is %d\n", rc);
+			exit(-1);
 		}
 	}
 
-	// Destroy threads / wait for other threads to finish.
 	pthread_attr_destroy(&attr);
-	for (int i = 0; i < threadGuard; i++) {
-		rc = pthread_join(thread[i], &threadStatus);
+	for (long t = 0; t < NUM_THREADS; t++) {
+		rc = pthread_join(thread[t], &status);
 		if (rc) {
-			cout << "ERROR: pthread_join() returned error: " << rc << "\n";
-			exit(1);
+			printf("ERROR; return code from pthread_join() is %d\n", rc);
+			exit(-1);
 		}
 	}
 	delete[] data;
-	delete[] thread;
 }
 
-/* On each iteration, finds a minimum-weight Minimum Spanning Tree
-   connecting a vertex v in the set A to the vertex u OUTSIDE of
-   set A using an implementation of Prim's algorithm.    */
 void TSP::findMST() {
-	int index[V];
-	bool MSTbuffer[V];
+	int key[V];
+	bool in_mst[V];
 	int parent[V];
 
-	for (int i = 0; i < V; i++) {
-		index[i] = INT_MAX;
-		MSTbuffer[i] = false;
+	for (int v = 0; v < V; v++) {
+		key[v] = std::numeric_limits<int>::max();
+		in_mst[v] = false;
 	}
 
-	// Root nodes.
-	index[0] = 0;
+	key[0] = 0;
 	parent[0] = -1;
 
-	// Find nearest node not yet in MST and add it.
-	for (int i = 0; i < V-1; i++) {
-		int v = nearestNextNode(index, MSTbuffer);
-		MSTbuffer[v] = true;
+	for (int i = 0; i < V - 1; i++) {
+		int v = nearestNextNode(key, in_mst);
+		in_mst[v] = true;
 		for (int u = 0; u < V; u++) {
-			if (graph[v][u] && MSTbuffer[u] == false && graph[v][u] < index[u]) {
+			if (graph[v][u] && in_mst[u] == false && graph[v][u] < key[u]) {
 				parent[u] = v;
-				index[u] = graph[v][u];
+				key[u] = graph[v][u];
 			}
 		}
 	}
-	// Update adjacency lists.
-	for (int i = 0; i < V; i++) {
-		int j = parent[i];
-		if (j != -1) {
-			adjacencyList[i].push_back(j);
-			adjacencyList[j].push_back(i);
+
+	for (int v1 = 0; v1 < V; v1++) {
+		int v2 = parent[v1];
+		if (v2 != -1) {
+			adjacencyList[v1].push_back(v2);
+			adjacencyList[v2].push_back(v1);
 		}
 	}
 };
 
-// Used by findMST - helper function to find the "nearest neighbor".
-int TSP::nearestNextNode(int *index, bool *MSTBuffer) {
-	int min = INT_MAX;
-	int minIndex;
+int TSP::nearestNextNode(int key[], bool mstSet[]) {
+	int min = std::numeric_limits<int>::max();
+	int min_index;
 	for (int v = 0; v < V; v++)
-		if (MSTBuffer[v] == false && index[v] < min) {
-			min = index[v];
-			minIndex = v;
+		if (mstSet[v] == false && key[v] < min) {
+			min = key[v];
+			min_index = v;
 		}
-	return minIndex;
+	return min_index;
 };
 
-/* Finds nodes with odd degree (odd adjacencyList count) and adds to
-   oddNodes vector.  Called by matchMST. */
 void TSP::findOddNodes() {
-	for (int i = 0; i < V; i++) {
-		if ((adjacencyList[i].size() % 2) != 0) {
-			oddNodes.push_back(i);
+	for (int r = 0; r < V; r++) {
+		if ((adjacencyList[r].size() % 2) != 0) {
+			oddNodes.push_back(r);
 		}
 	}
 }
 
-/* Finds "perfect matching" MST in the subgraph O using a greedy 
-   strategy, thus not nescessarily a minimum. Called from main(). */
 void TSP::matchMST() {
-	int closestNode;
-	int length;
-	vector<int>::iterator temp;
-	vector<int>::iterator first;
+	int closest, length; //int d;
+	std::vector<int>::iterator tmp, first;
 
 	findOddNodes();
 
 	while (!oddNodes.empty()) {
 		first = oddNodes.begin();
-		vector<int>::iterator iter;
+		vector<int>::iterator it = oddNodes.begin() + 1;
 		vector<int>::iterator end = oddNodes.end();
-		length = INT_MAX;
-		for (iter = oddNodes.begin()+1; iter != end; iter++) {
-			/* If iterated node is closer than closestNode,
-				update closestNode and length.*/
-			if (graph[*first][*iter] < length) {
-				length = graph[*first][*iter];
-				closestNode = *iter;
-				temp = iter;
+		length = std::numeric_limits<int>::max();
+		for (; it != end; ++it) {
+			if (graph[*first][*it] < length) {
+				length = graph[*first][*it];
+				closest = *it;
+				tmp = it;
 			}
-		}// Update adjacencyList and oddNodes.
-		adjacencyList[*first].push_back(closestNode);
-		adjacencyList[closestNode].push_back(*first);
-		oddNodes.erase(temp);
+		}
+		adjacencyList[*first].push_back(closest);
+		adjacencyList[closest].push_back(*first);
+		oddNodes.erase(tmp);
 		oddNodes.erase(first);
 	}
 }
 
-/* Determines a Euler path [uses every edge of a graph exactly 
-   once] from the inputed vector path and index.  Updates vector
-   path in place. */
-void TSP::eulerPath(int index, vector<int> &path) {
-	// Instantiate temp copy of original adjcencyList.
+
+void TSP::eulerCircuit(int pos, vector<int> &path) {
 	vector<int> *temp = new vector<int>[V];
-	// Resize temp to match adjacencyList and assign.
 	for (int i = 0; i < V; i++) {
 		temp[i].resize(adjacencyList[i].size());
 		temp[i] = adjacencyList[i];
 	}
-	// Since path is passed by reference:
+
 	path.clear();
 
-	// Utility stack.
-	stack<int> stack;
-	// Repeat until current vertex neighbors == 0 & stack empty.
-	while (temp[index].size() > 0 || !stack.empty()) {
-		// If current vertex has no neighbors
-		if (temp[index].size() == 0) {
-			path.push_back(index);
-			int last = stack.top();
-			stack.pop();
-			index = last;
+	stack<int> stk;
+	while (!stk.empty() || temp[pos].size() > 0) {
+		if (temp[pos].size() == 0) {
+			path.push_back(pos);
+			int last = stk.top();
+			stk.pop();
+			pos = last;
 		}
 		else {
-			stack.push(index);
-			int next2 = temp[index].back();
-			temp[index].pop_back();
-			for (unsigned int i = 0; i < temp[next2].size(); i++)
-				if (temp[next2][i] == index) {
-					temp[next2].erase(temp[next2].begin() + i);
+			stk.push(pos);
+			int neighbor = temp[pos].back();
+			temp[pos].pop_back();
+			for (unsigned int i = 0; i < temp[neighbor].size(); i++)
+				if (temp[neighbor][i] == pos) {
+					temp[neighbor].erase(temp[neighbor].begin() + i);
 					break;
 				}
-			index = next2;
+			pos = neighbor;
 		}
-	}// Get the last leg of the journey.
-	path.push_back(index);
-	delete[] temp;
+	}
+	path.push_back(pos);
 }
 
-/* Detemines a Hamiltonian path [visits each vertex exactly once] from the
-   previoulsy constructed Euler tour. [See euler() above.] Called from 
-   tourGen(). Int pathLength is updated by reference. */
-void TSP::hamiltonPath(vector<int> &path, int &pathLength) {
-	bool visited[V];
-	for (int i = 0; i < V; i++) {
-		visited[i] = false;
-	}
-	pathLength = 0;
-	int startAt = path.front();
-	vector<int>::iterator currNode = path.begin();
-	vector<int>::iterator nextNode = path.begin() + 1;
-	visited[startAt] = true;
 
-	while (nextNode != path.end()) {
-		if (!visited[*nextNode]) {
-			pathLength += graph[*currNode][*nextNode];
-			currNode = nextNode;
-			visited[*currNode] = true;
-			nextNode = currNode + 1;
+void TSP::hamiltonCircuit(vector<int> &path, int &path_dist) {
+	bool visited[V];
+	memset(visited, 0, V * sizeof(bool));
+
+	path_dist = 0;
+
+	int root = path.front();
+	vector<int>::iterator curr = path.begin();
+	vector<int>::iterator next = path.begin() + 1;
+	visited[root] = true;
+
+	while (next != path.end()) {
+		if (!visited[*next]) {
+			path_dist += graph[*curr][*next];
+			curr = next;
+			visited[*curr] = true;
+			next = curr + 1;
 		}
 		else {
-			nextNode = path.erase(nextNode);
+			next = path.erase(next);
 		}
-	}// Get the last leg of the journey.
-	pathLength += graph[*currNode][*nextNode];
-}
-
-/* Calls euler() by passing circuit vector then converts 
-   resulting euler path to a Hamiltonian path updating
-   circuit vector. */
-void TSP::tourGen(int index) {
-	eulerPath(index, circuit);
-	hamiltonPath(circuit, pathLength);
-}
-
-/* Calls eulerPath() and hamiltonPath() but passes a utiity vector
-   path for updating, thus finds a path length from a specified
-   node and returns it.  Called by Threads::run().     */
-int TSP::findBestPath(int index) {
-	vector<int>path;
-	eulerPath(index, path);
-
-	int length;
-	hamiltonPath(path, length);
-
-	for (int i = 0; i < RUN_SHORTER; i++) {
-		_2Opt(graph, path, length, V);
 	}
+	path_dist += graph[*curr][*next];
+}
+
+
+void TSP::tourGen(int pos) {
+	eulerCircuit(pos, circuit);
+	hamiltonCircuit(circuit, pathLength);
+}
+
+
+int TSP::findBestPath(int pos) {
+	vector<int>path;
+	eulerCircuit(pos, path);
+	int length;
+	hamiltonCircuit(path, length);
+
+	_2Opt(graph, path, length, V);
+	_2Opt(graph, path, length, V);
+	_2Opt(graph, path, length, V);
+	_2Opt(graph, path, length, V);
+	_2Opt(graph, path, length, V);
+
 	return length;
 }
 
-// Optimize solution circuit & pathLength.
-void TSP::call2Opt() {
-	_2Opt(graph, circuit, pathLength, V);
+void TSP::optimize() {
+	for (int i = 0; i < RUN_SHORTER; i++) {
+		_2Opt(graph, circuit, pathLength, V);
+	}
 }
 
-/* Swaps edges by removing edge 1 and edge 2 and reconnecting using
-   a new path. Begin and end are edge 1's v and edge 2's u.  Called 
-   from _2Opt() only and works with pathIsShorter(). */
-void TSP::edgeSwap(vector<int> &path, int begin, int end, int V) {
-	while (end - begin > 0) {
-		int temp = path[begin % V];
-		path[begin % V] = path[end % V];
-		path[end % V] = temp;
-		begin++;
+void TSP::edgeSwap(vector<int> &path, int start, int end, int n) {
+	while (end - start > 0) {
+		int temp = path[start % n];
+		path[start % n] = path[end % n];
+		path[end % n] = temp;
+		start++;
 		end--;
 	}
 }
 
-/* Returns appropriate bool if a shorter path between vertices is found and updates 
-   pathLength int as nescessary. Typically returns true if crossove condition exists.*/
-bool TSP::pathIsShorter(int **graph, int u1, int v1, int u2, int v2, int &pathLength) {
-	int dist1 = graph[u1][u2];
-	int dist2 = graph[v1][v2];
-	int dist3 = graph[u1][v1];
-	int dist4 = graph[u2][v2];
-	// If shorter, update int pathLength.
-	if ((dist1 + dist2) < (dist3 + dist4)) {
-		pathLength -= (dist3 + dist4 - dist1 - dist2);
-		return true;
+
+int TSP::pathIsShorter(int **graph, int v1, int v2, int v3, int v4, int &total_dist) {
+	if ((graph[v1][v3] + graph[v2][v4]) < (graph[v1][v2] + graph[v3][v4])) {
+		total_dist -= (graph[v1][v2] + graph[v3][v4] - graph[v1][v3] - graph[v2][v4]);
+		return 1;
 	}
-	return false;
+	return 0;
 }
 
-/* Takes a valid path and tests for shorter paths (preseumably ones that cross over each
-   other and reorders the path.  Utilizes helper functions pathIsShorter() and edgeSwap().*/
+
 int TSP::_2Opt(int **graph, vector<int> &path, int &pathLength, int V) {
-	int count = 0;
-	int priorDistance = pathLength;
+	int counter = 0;
+	int term_cond = 5;
+	int old_distance = pathLength;
 	int v1, v2, u1, u2;
 
 	for (int i = 0; i < V; i++) {
 		u1 = i;
 		v1 = (i + 1) % V;
+
 		for (int j = i + 2; (j + 1) % V != i; j++) {
+
 			u2 = j % V;
 			v2 = (j + 1) % V;
+
 			if (pathIsShorter(graph, path[u1], path[v1], path[u2], path[v2], pathLength)) {
-				edgeSwap(path, i+1, j, V);
-				if ((pathLength - priorDistance < RUN_SHORTER) && (count == RUN_SHORTER)) {
+
+				edgeSwap(path, i + 1, j, V); // v1, u2
+
+				if (pathLength - old_distance < 5 && counter == term_cond)
 					break;
-				}
-				if (pathLength - priorDistance > RUN_SHORTER) {
+
+				if (pathLength - old_distance > term_cond)
 					i = 0;
-				}
-				priorDistance = pathLength;
-				count++;
+
+				old_distance = pathLength;
+				counter++;
 			}
 		}
 	}
 	return pathLength;
 }
 
-/* Outputs the "optimal" pathLength and inorder circuit to outFileName.  */
 void TSP::printResult() {
 	ofstream outputStream;
 	outputStream.open(outFileName.c_str(), ios::out);
 	outputStream << pathLength << "\n";
-	for (vector<int>::iterator iter = circuit.begin(); iter != circuit.end(); iter++) {
-		outputStream << *iter << "\n";
+	for (vector<int>::iterator it = circuit.begin(); it != circuit.end(); ++it) {
+		outputStream << *it << "\n";
 	}
 	outputStream.close();
 };
